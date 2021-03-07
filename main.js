ad2ce8b92440e251f12c43b9526dd3359ad329d6
celestialheathen <jingchaoyu@hotmail.com>
2021-03-07 14:05:14 -0500

    test

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-const grids = document.querySelectorAll('td')
-const bombs = 4
+const table = document.querySelector('table#game-board')
 
-// Insert 4 bombs randomly to the grids, add class of .bomb to each one 
+const width = 12 
+const height = 11
+let cellID = 1
 
-const gridsArr = Array.from(grids)
+for (let h = 0; h < height; h++ ) {
 
+    // Create a table row
+    const tableRow = document.createElement('tr')
+    tableRow.id = h
 
-for (let i = 0; i < 4; i++) {
-    let rdnGrid = Math.round(Math.random() * grids.length)
-
-    while (grids[rdnGrid].classList.contains("bomb")) 
-    {
-        let rdnGrid = Math.round(Math.random() * grids.length)
+    // on each table row, create 12 table cells
+    for (let w = 0; w < width; w++) {
+        const td = document.createElement('td')
+        td.id = cellID++
+        td.classList.add('path')
+        tableRow.appendChild(td)
     }
     
-    grids[rdnGrid].classList.add("bomb")
+    table.appendChild(tableRow)
 }
+
+const cell1 = document.querySelector('td') 
+console.log(cell1)
